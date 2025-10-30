88c5bacf412a7f7d9f4a6ab726c6efeea670f157
JuanTajamar <juan.solis@tajamar365.com>
2025-10-30 11:41:44 +0100

    aaaa

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <CochesComponent/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CochesComponent from './components/CochesComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CochesComponent
   }
 }
 </script>
---
+++ src/components/CochesComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Api Coches</h1>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "CochesComponent"
+    }
+</script>
+
+<style>
+
+</style>
